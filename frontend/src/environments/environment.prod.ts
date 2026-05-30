0fe7d9431d7661be9711f44988f7decb40e1e39c
Patricia Mateica <patriciamateica33@gmail.com>
2026-05-30 22:33:16 +0300

    feature(FE):  so done

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://carebridge-production-dd9c.up.railway.app',
-  wsUrl: 'wss://carebridge-production-dd9c.up.railway.app/ws',
+  wsUrl: 'https://carebridge-production-dd9c.up.railway.app/ws',
   proxyBypass: false
 };
